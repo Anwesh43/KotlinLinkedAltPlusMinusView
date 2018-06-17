229a76965234bbc994cb93d5cd63dd102f5e7397
Anwesh43 <anweshthecool0@gmail.com>
2018-06-17 11:16:57 +0530

    adding LinkedAltPlusMinusView class

--- linkedaltplusminusview/src/main/java/com/anwesh/uiprojects/linkedaltplusminusview/LinkedAltPlusMinusView.kt
+++ linkedaltplusminusview/src/main/java/com/anwesh/uiprojects/linkedaltplusminusview/LinkedAltPlusMinusView.kt
@@ -4,3 +4,25 @@ package com.anwesh.uiprojects.linkedaltplusminusview
  * Created by anweshmishra on 17/06/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+import android.content.Context
+
+class LinkedAltPlusMinusView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
