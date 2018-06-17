a82414779abd46466411a897b3e0a02553413747
Anwesh43 <anweshthecool0@gmail.com>
2018-06-17 12:49:37 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedaltminusplusview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedaltminusplusview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedaltminusplusview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedaltplusminusview.LinkedAltPlusMinusView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedAltPlusMinusView.create(this)
     }
 }
